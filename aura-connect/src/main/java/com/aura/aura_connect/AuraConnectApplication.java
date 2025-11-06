b6d834c27977e491df8b37cc5c0e3ca422e3d520
구명회 <sl>
2025-11-06 12:36:53 +0900

    프로젝트 초기 구조 생성

---
+++ aura-connect/src/main/java/com/aura/aura_connect/AuraConnectApplication.java
@@ -0,0 +1,13 @@
+package com.aura.aura_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuraConnectApplication.class, args);
+	}
+
+}
